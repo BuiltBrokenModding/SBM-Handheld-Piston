094ad7a26c7070471dad1f0ebc55f97a519eb5c6
Robert WC Seifert <rseifert.phone@gmail.com>
2019-06-17 18:02:06 -0400

    More work on recipes

--- src/main/java/com/builtbroken/handheldpiston/item/ItemHandheldPiston.java
+++ src/main/java/com/builtbroken/handheldpiston/item/ItemHandheldPiston.java
@@ -35,7 +35,6 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
-import javax.vecmath.Vector3d;
 import java.util.HashSet;
 import java.util.Set;
 
